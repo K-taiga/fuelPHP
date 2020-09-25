64b7e939355631b049fa0d511db0af7564464a1d
Kobatai <taiga1983346@gmail.com>
2020-09-25 10:16:29 +0900

    add:Validation

--- sample/fuel/app/views/contact/index.php
+++ sample/fuel/app/views/contact/index.php
@@ -1,26 +1,42 @@
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" type="text/css" href="/style.css" />
-    <title>お問い合わせ</title>
-  </head>
-  <body>
-    <div id="wrap">
-      <div id="head">
-        <h1>お問い合わせ</h1>
-      </div>
+<!-- デフォルトはpostのform -->
+<?php print(Form::open('contact/index'));?>
+<table>
+  <tr>
+    <th><?php print(Form::label('お名前', 'myname'));?></th>
+    <td>
+      <?php print(Form::input('myname'));?>
+      <?php if ($val->error('myname')): ?>
+      <p class="error"><?php print($val->error('myname'));?></p>
+      <?php endif;?>
+    </td>
+  </tr>
+  <tr>
+    <th><?php print(Form::label('メールアドレス', 'email'));?></th>
+    <td>
+      <?php print(Form::input('email'));?>
+      <?php if ($val->error('email')): ?>
+      <p class="error"><?php print($val->error('email'));?></p>
+      <?php endif;?>
+    </td>
+  </tr>
+  <tr>
+    <th><?php print(Form::label('性別', 'gender'));?></th>
+    <!-- form_が接頭辞でつく -->
+    <td>
+      <?php print(Form::radio('gender', '男性', array('id' =>
+    'form_gender_male')));?>
+      <!-- 自動でform_がついてform_gender_maleになる -->
+      <?php print(Form::label('男性', 'gender_male'));?>
+      <?php print(Form::radio('gender', '女性', array('id' =>
+    'form_gender_female', 'checked')));?><?php print(Form::label('女性', 'gender_female'));?>
+    </td>
+  </tr>
+  <tr>
+    <th></th>
+    <td>
+      <?php print(Form::submit('submit', '登録する'));?>
+    </td>
+  </tr>
+</table>
 
-      <div id="content">
-        <!-- デフォルトはpostのform -->
-        <?php print(Form::open('contact/check')); ?>
-        <?php print(Form::label('お名前','myname')); ?>
-        <?php print(Form::input('myname')); ?>
-        <?php print(Form::submit('submit','登録する')); ?>
-        <?php print(Form::close()); ?>
-      </div>
-    </div>
-
-    <div id="foot"></div>
-  </body>
-</html>
+<?php print(Form::close());?>
