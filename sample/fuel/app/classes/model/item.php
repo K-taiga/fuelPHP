5f0035bdfd35aff17f3adec61544571c7484caba
Kobatai <taiga1983346@gmail.com>
2020-09-26 22:41:24 +0900

    add:exec oil model

--- sample/fuel/app/classes/model/item.php
+++ sample/fuel/app/classes/model/item.php
@@ -1,8 +1,26 @@
 <?php
-// Model_Crud = CRUD
-class Model_Item extends \Model_Crud {
-  // このModelが扱うtable名
-  protected static $_table_name = 'items';
-  // このtableの主キー
-  protected static $_primary_key = 'id';
+
+class Model_Item extends \Orm\Model
+{
+	protected static $_properties = array(
+		'id',
+		'item_name',
+		'price',
+		'created_at',
+		'updated_at',
+	);
+
+	protected static $_observers = array(
+		'Orm\Observer_CreatedAt' => array(
+			'events' => array('before_insert'),
+			'mysql_timestamp' => false,
+		),
+		'Orm\Observer_UpdatedAt' => array(
+			'events' => array('before_update'),
+			'mysql_timestamp' => false,
+		),
+	);
+
+	protected static $_table_name = 'items';
+
 }
